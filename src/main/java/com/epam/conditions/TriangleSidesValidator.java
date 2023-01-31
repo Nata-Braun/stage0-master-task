cb4c34353f08c958a59b41e23f004d2d9344bc49
Nata Braun <pushistik2030@gmail.com>
2023-01-31 20:58:19 +0300

    In process

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,11 +3,12 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
     private double firstSide; private double secondSide; private double thirdSide;
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        if (firstSide<=0||secondSide<=0||thirdSide<=0) {
+        if (firstSide<=0||secondSide<=0||thirdSide<=0 || firstSide+secondSide<=thirdSide||secondSide+thirdSide<=firstSide||thirdSide+firstSide<=secondSide) {
             System.out.println("it's not a triangle");
         }else {
             System.out.println("this is a valid triangle");
         }
+
     }
 
 }
