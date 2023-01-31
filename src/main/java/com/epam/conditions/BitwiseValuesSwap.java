ef3c448e6b5a98ad4ea8906439bc2a6284db86df
Nata Braun <pushistik2030@gmail.com>
2023-01-31 22:01:08 +0300

    In process

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -1,8 +1,9 @@
 package com.epam.conditions;
 
 public class BitwiseValuesSwap {
-
+private int first; private int second;
     public void swap(int first, int second) {
+
     }
 
 }
--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -1,8 +1,21 @@
 package com.epam.conditions;
 
 public class IntegerDivider {
-
+    private int dividend; private int divider;
     public void printCompletelyDivided(int dividend, int divider) {
+        int res = 0;
+        if (divider!=0){
+            res = dividend / divider;
+        } else {
+            System.out.println("division by zero");
+            return;
+        }
+        if (divider*res == dividend) {
+            System.out.println("can be divided completely");
+        }else {
+            System.out.println("cannot be divided completely");
+
+        }
     }
 
 }
